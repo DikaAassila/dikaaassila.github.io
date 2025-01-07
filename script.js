a29ef8a3eff7a45cb5614c5d0dbc7376f2d0efd0
DikaAassila <aassila@dicastalma.com>
2025-01-07 14:05:32 +0100

    v1.2

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-function redirectAfterDelay() {
-  setTimeout(() => {
-    window.location.href = "https://www.dicastal.com/en"; // Replace with your target URL
-  }, 2000); // 2 seconds delay
+function startCountdown() {
+  const loaderText = document.getElementById("loader-text");
+  let countdown = 5;
+
+  const countdownInterval = setInterval(() => {
+    if (countdown > 0) {
+      loaderText.innerText = `Redirecting the Company's Group Website in ${countdown}`;
+      countdown--;
+    } else {
+      loaderText.innerText = "Redirecting, please wait...";
+      clearInterval(countdownInterval);
+      window.location.href = "https://www.dicastal.com/en"; // Replace with your target URL
+    }
+  }, 1000); // Update every second
 }
 
 // Start the redirection once the page is loaded
-document.addEventListener("DOMContentLoaded", redirectAfterDelay);
+document.addEventListener("DOMContentLoaded", startCountdown);
